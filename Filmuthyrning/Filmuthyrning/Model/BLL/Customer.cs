9bfd1cdcd31cdf1ac855394717bccf851cbfc531
Henrik Gabrielsson <hg222aa@student.lnu.se>
2014-03-26 10:10:19 +0100

    validering funkar lite bättre

--- Filmuthyrning/Filmuthyrning/Model/BLL/Customer.cs
+++ Filmuthyrning/Filmuthyrning/Model/BLL/Customer.cs
@@ -9,21 +9,19 @@ namespace Filmuthyrning.Model.BLL
     public class Customer
     {
 
-        [Required(ErrorMessage = "En kund måste väljas.")]
         public int CustomerID { get; set; }
-
         public int CustomerTypeID { get; set; }
 
         [Required(ErrorMessage = "Kunden måste ha ett förnamn.")]
-        [MaxLength(ErrorMessage="Förnamnet får inte vara längre än 50 tecken.")]
+        [StringLength(50, ErrorMessage = "Förnamnet får inte vara längre än 50 tecken.")]
         public string FirstName { get; set; }
 
         [Required(ErrorMessage = "Kunden måste ha ett efternamn.")]
-        [MaxLength(ErrorMessage = "Efternamnet får inte vara längre än 50 tecken.")]
+        [StringLength(50, ErrorMessage = "Efternamnet får inte vara längre än 50 tecken.")]
         public string LastName { get; set; }
 
         [Required(ErrorMessage = "Kunden måste ha ett telefonnummer.")]
-        [MaxLength(ErrorMessage = "Telefonnumret får inte vara längre än 10 tecken.")]
+        [StringLength(10, ErrorMessage = "Telefonnumret får inte vara längre än 10 tecken.")]
         [RegularExpression("^[0-9]{1,10}$",ErrorMessage="Telefonnumret får bara bestå av siffror")]
         public string PhoneNumber { get; set; }  
 
